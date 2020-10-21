f0baddfd78abdef74061afa73432f7f02d16c77b
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-10-21 23:28:36 +0200

    update

--- src/main/kotlin/dashboard/config/GeneralConfig.java
+++ src/main/kotlin/dashboard/config/GeneralConfig.java
@@ -1,45 +1,39 @@
 package dashboard.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class GeneralConfig {
 
-    @Value("${database.username}")
-    private String username;
-
-    @Value("${database.password}")
-    private String password;
-
-    @Value("${database.classURL}")
-    private String driverClassURL;
-
-
-    @Profile("PROD")
-    @Bean
-    public DataSource postgresDataSource(){
-        DriverManagerDataSource datasource = new DriverManagerDataSource();
-        datasource.setDriverClassName(driverClassURL);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        return datasource;
-    }
-
-    @Profile("PROD")
-    @Bean
-    public DataSource h2DataSource(){
-        DriverManagerDataSource datasource = new DriverManagerDataSource();
-        datasource.setDriverClassName(driverClassURL);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        return datasource;
-    }
+//    @Value("${database.username}")
+//    private String username;
+//
+//    @Value("${database.password}")
+//    private String password;
+//
+//    @Value("${database.classURL}")
+//    private String driverClassURL;
+//
+//
+//    @Profile("PROD")
+//    @Bean
+//    public DataSource postgresDataSource(){
+//        DriverManagerDataSource datasource = new DriverManagerDataSource();
+//        datasource.setDriverClassName(driverClassURL);
+//        datasource.setUsername(username);
+//        datasource.setPassword(password);
+//        return datasource;
+//    }
+//
+//    @Profile("PROD")
+//    @Bean
+//    public DataSource h2DataSource(){
+//        DriverManagerDataSource datasource = new DriverManagerDataSource();
+//        datasource.setDriverClassName(driverClassURL);
+//        datasource.setUsername(username);
+//        datasource.setPassword(password);
+//        return datasource;
+//    }
 
 //    @Bean
 //    MultipartConfigElement multipartConfigElement() {
